a87555ad61a0a751d138d82c6a90fdff06b1978e
patrikmasiar <masiar.patrik@gmail.com>
2021-09-01 15:45:19 +0200

    Add user condition, create SignedIn component

---
+++ components/SignedIn.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import { Space } from '@supabase/ui';
+
+const SignedIn: FC = () => {
+  return (
+    <Space direction="vertical" size={6}>
+    </Space>
+  )
+};
+
+export default SignedIn;
--- components/View.tsx
+++ components/View.tsx
@@ -1,10 +1,16 @@
+import { Auth } from '@supabase/ui';
 import { FC } from 'react';
 import AuthUser from './AuthUser';
+import SignedIn from './SignedIn';
 
 const View: FC = () => {
-  return (
-    <AuthUser />
-  );
+  const { user } = Auth.useUser();
+
+  if (!user) {
+    return <AuthUser />
+  }
+
+  return <SignedIn />;
 };
 
 export default View;
