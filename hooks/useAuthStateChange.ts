6a4886cfb5565d729990273690c21404b2b11784
patrikmasiar <masiar.patrik@gmail.com>
2021-09-01 16:08:01 +0200

    Create authListener

--- hooks/useAuthStateChange.ts
+++ hooks/useAuthStateChange.ts
@@ -1,10 +1,33 @@
+import { Auth } from '@supabase/ui';
 import { useState, useEffect } from 'react';
+import { supabase } from '../utils/initSupabase';
 
 export const useAuthStateChange = () => {
   const [authView, setAuthView] = useState<AuthView>(AuthView.SIGN_IN);
 
   useEffect(() => {
-    return () => {};
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        if (event === 'PASSWORD_RECOVERY') {
+          setAuthView(AuthView.UPDATE_PASSWORD)
+        }
+
+        if (event === 'USER_UPDATED') {
+          setTimeout(() => setAuthView(AuthView.SIGN_IN), 1000);
+        }
+          
+        fetch('/api/auth', {
+          method: 'POST',
+          headers: new Headers({ 'Content-Type': 'application/json' }),
+          credentials: 'same-origin',
+          body: JSON.stringify({ event, session }),
+        }).then((res) => res.json())
+      }
+    )
+
+    return () => {
+      authListener?.unsubscribe();
+    };
   }, []);
 
   return {
